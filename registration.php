8738a04e46b0589ce2ec8089d4df5d45a2f62ab4
Arshad Muhammad <amuh@ciklum.com>
2020-03-21 16:32:37 +0500

    initial commet

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amuh
+ * Date: 3/21/2020
+ * Time: 3:28 PM
+ */
+
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'MagArs_DigitalCodes',
+    __DIR__
+);
